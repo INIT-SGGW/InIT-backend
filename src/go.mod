4b378949e666ccac2cab032d98b0391eb91beeb9
Krzysztof Rozbicki <krzysz.rozbicki@gmail.com>
2024-11-29 21:51:42 +0100

    Add initial setup for project

---
+++ src/go.mod
@@ -0,0 +1,13 @@
+module INIT-SGGW/InIT-backend
+
+go 1.23.3
+
+require (
+	github.com/danielgtaylor/huma/v2 v2.26.0
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.1.0
+	go.uber.org/zap v1.27.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+)
+
+require go.uber.org/multierr v1.11.0 // indirect
---
+++ src/server/init-server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"INIT-SGGW/InIT-backend/initializer"
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humachi"
+	"github.com/go-chi/chi/v5"
+)
+
+type HiResponse struct {
+	Body struct {
+		Message string `json:"message" example:"Hello KN InIT!" doc:"Greeting message"`
+	}
+}
+
+func main() {
+	logger := initializer.CreateLogger()
+	fmt.Println("Hello InIT!")
+	r := chi.NewRouter()
+	r.Use(initializer.New(logger))
+
+	api := humachi.New(r, huma.DefaultConfig("KN INIT Website API", "1.0.0"))
+
+	huma.Get(api, "/hi", func(ctx context.Context, input *struct{}) (*HiResponse, error) {
+		resp := &HiResponse{}
+		resp.Body.Message = "Hello KN InIT!"
+		return resp, nil
+	})
+
+	http.ListenAndServe(":3131", r)
+}
