99800f79839683e77db9df2a075533f8b475387c
Krzysztof Rozbicki <krzysz.rozbicki@gmail.com>
2024-12-22 23:56:58 +0100

    Add register endpoint example

--- src/go.mod
+++ src/go.mod
@@ -1,4 +1,4 @@
-module INIT-SGGW/InIT-backend
+module INIT-SGGW/InIT-backend-00.Gateway
 
 go 1.23.3
 
@@ -7,7 +7,16 @@ require (
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.1.0
 	go.uber.org/zap v1.27.0
+	google.golang.org/genproto v0.0.0-20241219192143-6b3ec007d9bb
+	google.golang.org/grpc v1.69.0
+	google.golang.org/protobuf v1.36.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
 
-require go.uber.org/multierr v1.11.0 // indirect
+require (
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241209162323-e6fa225c2576 // indirect
+)
--- src/server/init-server.go
+++ src/server/init-server.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	"INIT-SGGW/InIT-backend/initializer"
+	"INIT-SGGW/InIT-backend-00.Gateway/initializer"
+	"INIT-SGGW/InIT-backend-00.Gateway/model"
 	"context"
 	"fmt"
+	"log"
 	"net/http"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "INIT-SGGW/InIT-backend-00.Gateway/proto-messages/protogen/register"
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humachi"
@@ -13,7 +20,7 @@ import (
 
 type HiResponse struct {
 	Body struct {
-		Message string `json:"message" example:"Hello KN InIT!" doc:"Greeting message"`
+		Message string `json:"message" example:"I'm Alive!" doc:"Health check"`
 	}
 }
 
@@ -23,27 +30,46 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(initializer.New(logger))
 
+	standardApiRouter := chi.NewRouter()
+
+	r.Mount("/v1/api", standardApiRouter)
+
 	api := humachi.New(r, huma.DefaultConfig("KN INIT Website API", "1.0.0"))
 
-	huma.Get(api, "/hi", func(ctx context.Context, input *struct{}) (*HiResponse, error) {
+	huma.Get(api, "/hearthbeat", func(ctx context.Context, input *struct{}) (*HiResponse, error) {
 		resp := &HiResponse{}
-		resp.Body.Message = "Hello KN InIT!"
+		resp.Body.Message = "I'm Alive!"
 		return resp, nil
 	})
 
 	huma.Register(api, huma.Operation{
-		OperationID: "welcome-user",
-		Method:      http.MethodGet,
-		Path:        "/hi/{name}",
-		Summary:     "Great user",
-		Description: "Great user with his name",
-	}, func(ctx context.Context, input *struct {
-		Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
-	}) (*HiResponse, error) {
-		resp := &HiResponse{}
-		resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
+		OperationID: "register-user",
+		Method:      http.MethodPost,
+		Path:        "/api/v1/register/user",
+		Summary:     "Register user",
+		Description: "Register user and send confirmation email to provided adress with unique token for account verification",
+	}, func(ctx context.Context, input *model.RegisterUserRequest) (*model.RegisterUserResponse, error) {
+		resp := &model.RegisterUserResponse{}
+		resp.Body.Status = "created"
 		return resp, nil
 	})
+
+	conn, err := grpc.NewClient("localhost:8089", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	_ = pb.NewRegisterUserSenderClient(conn)
+	grpc.WithTransportCredentials(insecure.NewCredentials())
+
+	// for i := 0; i < 10; i++ {
+	// 	resp, err := client.SendRegisterUserRequestSession(context.Background(), &pb.RegisterUserRequestRPC{RequestSessionId: int32(i), FirstName: "Adrian", IsAggrementFulfielled: i%2 == 0})
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Println(resp)
+	// }
 
 	http.ListenAndServe(":3131", r)
 }
