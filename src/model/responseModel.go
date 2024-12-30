dfc1f9eeb36fa03628fc267a99efce90de672b69
Krzysztof Rozbicki <krzysz.rozbicki@gmail.com>
2024-12-30 22:05:24 +0100

    Change the response model

--- src/model/responseModel.go
+++ src/model/responseModel.go
@@ -1,7 +1,8 @@
 package model
 
 type RegisterUserResponse struct {
-	Body struct {
+	Status int
+	Body   struct {
 		Status string `json:"status" example:"created" doc:"Status of creating user request"`
 		Error  string `json:"error,omitempty" example:"user already exist" doc:"Errors in user creation"`
 	}
